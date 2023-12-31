cdf6f875c253af118f460474761b0cdd99c098d2
Taicho1 <122099046+Taicho1@users.noreply.github.com>
2023-12-31 16:21:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add this to your JavaScript file
+function toggleNavbar() {
+    const navbarItems = document.getElementById('navbar-items');
+    navbarItems.style.display === 'none' ? navbarItems.style.display = 'block' : navbarItems.style.display = 'none';
+   }
